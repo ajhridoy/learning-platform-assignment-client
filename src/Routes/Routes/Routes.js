acc2b235a9ae697c67a679bd4df769c6efaa7b13
ajhridoy <ajhridoy461@gmail.com>
2022-10-25 03:32:12 +0600

    create a responsive navbar

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layouts/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Courses from "../../Pages/Courses/Courses";
+import Faq from "../../Pages/Faq/Faq";
+import Home from "../../Pages/Header/Home/Home/Home";
+import Signin from "../../Pages/Signin/Signin";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element:<Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/signin',
+                element: <Signin></Signin>
+            },
+        ]
+    }
+])
